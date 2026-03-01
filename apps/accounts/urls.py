43de405063d32f9aa47a6e6d1beca56f5a5974c4
Ephraim <ephraimakolo2017@gmail.com>
2026-03-01 21:11:09 +0100

    expose url

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('otp/request', views.OTPRequestView.as_view(), name='otp-request'),
+    path('otp/verify', views.OTPVerifyView.as_view(), name='otp-verify'),
 ]
