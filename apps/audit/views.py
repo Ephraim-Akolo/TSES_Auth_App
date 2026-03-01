9df6711316f73e12b7bc6cb84463c90250414814
Ephraim <ephraimakolo2017@gmail.com>
2026-03-01 21:45:53 +0100

    add views

--- apps/audit/views.py
+++ apps/audit/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics, filters as drf_filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import AuditLog
+from .serializers import AuditLogsSerializer
+from .filters import AuditLogFilter
 
-# Create your views here.
+
+class AuditLogsView(generics.ListAPIView):
+    queryset = AuditLog.objects.all()
+    serializer_class = AuditLogsSerializer
+    filter_backends = [DjangoFilterBackend, drf_filters.OrderingFilter]
+    filterset_class = AuditLogFilter
+    ordering_fields = ['created_at', 'event']
+    
